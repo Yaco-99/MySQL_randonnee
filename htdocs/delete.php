9889f6b9b1e493f79357db8ea0bd7b099822e0ac
Yaco-99 <coopman.yannick@gmail.com>
2020-11-30 12:58:09 +0100

    delete location fix

--- htdocs/delete.php
+++ htdocs/delete.php
@@ -4,4 +4,4 @@ require "connect.php";
 $id = $_GET['id'];
 $sql = $pdo->prepare('DELETE from hiking WHERE id=?');
 $sql->execute(array($id));
-header("location: http://localhost:2021/read.php");
+header("location: read.php");
